920a93243bec8403485099d3bd0a877bbe7ab3b5
amitiamity <amitptl.in@gmail.com>
2022-04-08 11:48:22 +0400

    added new test

--- src/main/java/com/test/gradle/java/App.java
+++ src/main/java/com/test/gradle/java/App.java
@@ -10,6 +10,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        System.out.println("hello1");
+        System.out.println("hello2");
     }
 }
